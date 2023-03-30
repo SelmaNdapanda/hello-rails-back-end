4620b7847d9ffdb05b85c178b47e95bcbc11e359
Selma <selmandapanda@yahoo.com>
2023-03-30 20:48:55 +0200

    Update random message controller

--- app/controllers/api/v1/random_controller.rb
+++ app/controllers/api/v1/random_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::RandomController < ApplicationController
   def index
-    @random_message = Message.random
-    render json: @random_message
+    random_message = Message.order('Random()').first
+    if random_message
+      render json: random_message
+    else
+      render json: { random_message: 'There is no random message' }
+    end
   end
 end
