9b3facb142036f8d0c71d8db637bccbc7aa8f69b
Selma <selmandapanda@yahoo.com>
2023-03-30 21:47:29 +0200

    Generate API documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   namespace :api do
     namespace :v1 do
       resources :random
